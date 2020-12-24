b109bb6211a36a9d0a27954b704c22810faba16f
qytiz <qytizz@gmail.com>
2020-12-24 16:40:08 +0300

    FIXING ISSUE

--- db/migrate/20201210151707_create_answers.rb
+++ db/migrate/20201210151707_create_answers.rb
@@ -3,11 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
     create_table :answers do |t|
       t.string :title, null: false
       t.boolean :correct, default: false
-<<<<<<< Updated upstream
-      t.references :questions, null: false, foreign_key: true
-=======
       t.references :question, null: false, foreign_key: true
->>>>>>> Stashed changes
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2020_12_10_151817) do
-
-  create_table "answers", force: :cascade do |t|
-    t.string "title", null: false
-    t.boolean "correct", default: false
-    t.integer "questions_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["questions_id"], name: "index_answers_on_questions_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "tests_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tests_id"], name: "index_questions_on_tests_id"
-=======
 ActiveRecord::Schema.define(version: 2020_12_21_121848) do
 
   create_table "answers", force: :cascade do |t|
@@ -67,27 +42,17 @@ ActiveRecord::Schema.define(version: 2020_12_21_121848) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
->>>>>>> Stashed changes
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
-<<<<<<< Updated upstream
-    t.integer "categories_id", null: false
-    t.integer "users_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["categories_id"], name: "index_tests_on_categories_id"
-    t.index ["users_id"], name: "index_tests_on_users_id"
-=======
     t.integer "category_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["user_id"], name: "index_tests_on_user_id"
->>>>>>> Stashed changes
   end
 
   create_table "users", force: :cascade do |t|
@@ -96,17 +61,10 @@ ActiveRecord::Schema.define(version: 2020_12_21_121848) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< Updated upstream
-  add_foreign_key "answers", "questions", column: "questions_id"
-  add_foreign_key "questions", "tests", column: "tests_id"
-  add_foreign_key "tests", "categories", column: "categories_id"
-  add_foreign_key "tests", "users", column: "users_id"
-=======
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users"
->>>>>>> Stashed changes
 end
