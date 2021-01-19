1a348d2456b8a992e84fa6f5bf13b4cb07f3d9c3
qytiz <66452104+qytiz@users.noreply.github.com>
2021-01-19 14:53:10 +0300

    fixing and changing db

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,19 +3,19 @@
 class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :category
- 
+
   has_many :questions, dependent: :destroy
   has_many :test_passeges, dependent: :destroy
-  has_many :passing_users, through: :test_passeges, source: :user
+  has_many :passing_users, dependent: :destroy, through: :test_passeges, source: :user
 
   validates :title, :level, presence: true
   validates :level, numericality: { only_integer: true, greater_than: 0 }
   validates :title, uniqueness: { scope: :level, message: 'Идентичный тест уже существует' }
 
-  scope :easy, -> {where(level: (0..1))} 
-  scope :medium, -> {where(level: (2..4))} 
-  scope :hard, -> {where(level: (5..Float::INFINITY))}
-  scope :from_category, ->(category) { joins(:category).where(categories: { title: category }).order(title: :desc)) }
+  scope :easy, -> { where(level: (0..1)) }
+  scope :medium, -> { where(level: (2..4)) }
+  scope :hard, -> { where(level: (5..Float::INFINITY)) }
+  scope :from_category, ->(category) { joins(:category).where(categories: { title: category }).order(title: :desc) }
 
   def self.tests_with_category(category)
     Test.from_category(category).pluck(:title)
