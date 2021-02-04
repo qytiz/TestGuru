bc37675528eb1d5a0b6533cd52e6685f4543225e
qytiz <66452104+qytiz@users.noreply.github.com>
2021-02-04 13:00:56 +0300

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
     def current_year
         Time.current.year
     end
-    
     def github_url(author,repo)
         link_to repo,"https://github.com/#{author}/#{repo}",target: :_blank, rel: 'nofollow'
     end
