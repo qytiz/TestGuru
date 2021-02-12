ec9ed14c895184988b01a6bc06bb273e77820254
qytiz <66452104+qytiz@users.noreply.github.com>
2021-02-12 19:43:59 +0300

    v 0.10.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,20 +8,14 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    attributes = %i[first_name last_name]
-    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
 
   def after_sign_in_path_for(resource)
-    if resource.is_a?(Admin)
-      if stored_location_for(resource)=='/'
-        admin_tests_path
-      else
-        stored_location_for(resource)
-      end
+    stored_location_for(resource)|| if resource.is_a?(Admin)
+      admin_tests_path
     else
-    stored_location_for(resource)
-    super
+      super
     end
   end
 end
